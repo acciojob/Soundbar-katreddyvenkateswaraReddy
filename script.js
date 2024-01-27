48b19ff24878400c5600d4cac61f4f8870c20678
katreddyvenkateswaraReddy <63310358+katreddyvenkateswaraReddy@users.noreply.github.com>
2024-01-27 12:29:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,17 @@
 //your JS code here. If required.
-let myaudio=new Audio();
+const Buttons = document.querySelectorAll(".btn");
+const StopBtn = document.querySelector(".stop");
 
-function applause(){
-	myaudio.src= "./sounds/sound1";
-	myaudio.currentTime=0;
-	myaudio.play();
-	
-}
-function boo() {
-	myaudio.src="./sounds/sound2";
-	myaudio.currentTime=0;
-	myaudio.play();
-}
-function gasp() {
-	myaudio.src="./sounds/sound3";
-	myaudio.currentTime=0;
-	myaudio.play();
-}
-function tada() {
-	myaudio.src="./sounds/sound4";
-	myaudio.currentTime=0;
-	myaudio.play();
+Buttons.forEach(x =>{
+	x.addEventListener("click",() =>{
+		const Audio = x.querySelector("audio");
+		Audio.play();
+	});
+});
 
-}
-function victory() {
-	myaudio.src="./sounds/sound5";
-	myaudio.currentTime=0;
-	myaudio.play();
-}
-function wrong() {
-	myaudio.src="./sounds/sound6";
-	myaudio.currentTime=0;
-	myaudio.play();
-}
-function stop() {
-	myaudio.src="./sounds/sound7";
-	myaudio.currentTime=0;
-	myaudio.play();
-}
-
-function stop() {
-	myaudio.pause();
-	
-}
+StopBtn.addEventListener("click",() =>{
+	const Audios = document.querySelectorAll("audio");
+	Audios.forEach(y,() =>{
+		y.pause();
+	});
+});
